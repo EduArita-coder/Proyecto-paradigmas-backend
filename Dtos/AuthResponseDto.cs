7f9be40dd6aa173162c78e329da871b9875335a6
qndree <danisuarezclaudino013@gmail.com>
2026-08-01 13:09:21 -0600

    Implementar IProductService y DTOs de autenticacion y validaciones

---
+++ Dtos/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+namespace GAMEHOSTING_APIREST.Dtos;
+
+public class AuthResponseDto
+{
+    public string Token { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+}
--- Services/Interfaces/IProductService.cs
+++ Services/Interfaces/IProductService.cs
@@ -1,7 +1,11 @@
+using GAMEHOSTING_APIREST.Dtos;
+
 namespace GAMEHOSTING_APIREST.Services.Interfaces
 {
     public interface IProductService
     {
-        
+        Task<List<ProductDto>> GetAllAsync();
+        Task<ProductDto?> GetByIdAsync(Guid id);
+        Task<ProductDto> CreateAsync(CreateProductDto dto);
     }
 }
